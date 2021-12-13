d226393087be93ebf9120c176ed50f38c3127263
kshitij3027 <kshitijskakade307@gmail.com>
2021-12-14 00:33:10 +0530

    Set Up Basic Twich Channel App
    Set up the Client and Channel Components of the GetStream API and used
    their documentation examples for setting up a functional running App
    While running app initially with dummy channel members an error indicating
    "role channel_member is not allowed to access Resource Create_Channel" is
    encountered. To solve this simply go on the dashboard->lower nav menu->
    click on chat->go to the channel type you set up while initializing the
    channel for your app and click on it->Under the permissions section you
    will see a huge array of action,resources,roles etc. select the item with
     role as channel_member and paste the string "Create_Channel" in the
     resources array at the end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+
+import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+import '@stream-io/stream-chat-css/dist/css/index.css';
+
+const filters = { type: 'gaming' };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const client = StreamChat.getInstance('nbccrq6sq9fh');
+
+const App = () => {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel,setChannel] = useState(null)
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: 'trey-anastasio',
+            name: 'Trey Anastasio',
+          },
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHJleS1hbmFzdGFzaW8ifQ.M-L9xFTTRbpldOOpiOHeMssXNkWXTUrer9EY7AvPovM',
+        );
+        const channel = await client.channel('gaming', 'gaming-demo', {
+          name: 'My Gaming App',
+          members: ['dave-matthews', 'trey-anastasio'],
+        })
+        setChannel(channel);
+
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client} darkMode={true}  >
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
-}
+};
 
 export default App;
